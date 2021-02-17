1a5899c89f1c0faaf6d166f2df4e9b5dcb355f05
Andrew Lee <andrewleee10@gmail.com>
2021-02-17 15:33:47 -0800

    code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+document.getElementById('timer').textContent = localStorage.getItem('time')
+
 document.getElementById('startBtn').addEventListener('click', event => {
   event.preventDefault()
 
+  document.location.href = './question1.html'
   let time = 75
   setInterval(() => {
-    
-  })
+    time--
+    document.getElementById('timer').textContent = `Time: ${time}`
+    localStorage.setItem('time', time)
+  }, 1000)
 })
