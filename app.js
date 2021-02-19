75207165255382c7a911a0dc550295d3740f3212
Andrew Lee <andrewleee10@gmail.com>
2021-02-18 17:29:05 -0800

    new approach

--- app.js
+++ app.js
@@ -1,126 +1,148 @@
-let time = 75
+let count = 75
 let score = 0
+let questionPos = 0
 
-// document.getElementById('startBtn').addEventListener('click', event => {
-//   event.preventDefault()
+let quizQuestions = [
+  `<h2>Commonly used data types DO NOT include:</h2>
+  <div id="btns">
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">A. Strings</button>
+        <button class="ansChoice wrong">B. Booleans</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice correct">C. Alerts</button>
+        <button class="ansChoice wrong">D. Numbers</button>
+      </div>
+    </div>
+  </div>`,
+  `<h2>The condition in an if/else statement is enclosed within ___.</h2>
+  <div id="btns">
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">A. Quotes</button>
+        <button class="ansChoice wrong">B. Curly Brackets</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice correct">C. Parentheses</button>
+        <button class="ansChoice wrong">D. Square Brackets</button>
+      </div>
+    </div>
+  </div>`,
+  `<h2>Arrays in JavaScript can be used to store ___.</h2>
+  <div id="btns">
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">A. Numbers and Strings</button>
+        <button class="ansChoice wrong">B. Other Arrays</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">C. Booleans</button>
+        <button class="ansChoice correct">D. All of the Above</button>
+      </div>
+    </div>
+  </div>`,
+  `<h2>String values must be enclosed wihtin ___ when being assigned to variables</h2>
+  <div id="btns">
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">A. Commas</button>
+        <button class="ansChoice wrong">B. Curly Brackets</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice corect">C. Quotes</button>
+        <button class="ansChoice wrong">D. Parentheses</button>
+      </div>
+    </div>
+  </div>`,
+  `<h2>A very useful tool used during development and debugging for printing content to the debugger is:</h2>
+  <div id="btns">
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">A. JavaScript</button>
+        <button class="ansChoice wrong">B. Terminal/Bash</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <button class="ansChoice wrong">C. For Loops</button>
+        <button class="ansChoice correct">D. console.log</button>
+      </div>
+    </div>
+  </div>`
+]
 
-//   document.location.href = './question1.html'
-//   let time = 75
-//   setInterval(() => {
-//     time--
-//     document.getElementById('timer').textContent = `Time: ${time}`
-//     localStorage.setItem('time', time)
-//   }, 1000)
-// })
+let finishPage = [
+  `<h2>All done!</h2>
+  <form>
+    <p>
+      <label for="initial">Initials: </label>
+      <input type="text" id="initials">
+    </p>
+    <button type="submit" id="finish">Submit</buttton>
+  </form>
+  `,
+  `
+  <h2>Highscores:</h2>
+  <p id="scoreElem">
+    ${localStorage.getItem('initials')} - ${score}
+  </p>
+  <p>
+    <button id="clear">Clear</button>
+    <button id="highscores>Highscores</button>
+  </p>
+  `
+]
 
-document.getElementById('btns1').addEventListener('click', event => {
-  if(event.target.className === 'ansChoice3') {
-    document.getElementById('ans1').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans1').append('Correct!')
-    score++
-    setInterval(() => {
-      document.getElementById('ans1').textContent = ''
-    }, 3000);
-  } else {
-    document.getElementById('ans1').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans1').append('Wrong!')
-    setInterval(() => {
-      document.getElementById('ans1').textContent = ''
-    }, 3000);
-  }
+
+document.getElementById('scoreElem').localStorage.getItem('initials')
+
+  // Timer
+document.getElementById('startBtn').addEventListener('click', () => {
+  
+  setInterval(() => {
+    count--
+    document.getElementById('timer').innerHTML = `Time: ${count}`
+  }, 1000);
+
+  // Start quiz
+  document.getElementById('quiz').innerHTML = quizQuestions[questionPos]
+
+  document.addEventListener('click', event => {
+
+    if(event.target.classList.contains('ansChoice')) {
+      if(questionPos < ((quizQuestions.length)-1)) {
+        questionPos++
+        document.getElementById('quiz').innerHTML = quizQuestions[questionPos]
+
+        if(event.target.classList.contains('wrong')) {
+          count -= 10
+        }
+        if (event.target.classList.contains('correct')){
+          score += 1
+        } 
+      }
+      else {
+        document.getElementById('quiz').innerHTML = finishPage[0]
+
+        document.getElementById('finish').addEventListener('click', event => {
+          event.preventDefault()
+
+          console.log(score)
+          let init = localStorage.setItem('initials', document.getElementById('initials').value)
+          document.getElementById('quiz').innerHTML = finishPage[1]
+          })
+        }
+      }
+  })
 })
 
-document.getElementById('btns2').addEventListener('click', event => {
-  if (event.target.className === 'ansChoice3') {
-    document.getElementById('ans2').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans2').append('Correct!')
-    score++
-    setInterval(() => {
-      document.getElementById('ans2').textContent = ''
-    }, 3000);
-  } else {
-    document.getElementById('ans2').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans2').append('Wrong!')
-    setInterval(() => {
-      document.getElementById('ans2').textContent = ''
-    }, 3000);
-  }
-})
-
-document.getElementById('btns3').addEventListener('click', event => {
-  if (event.target.className === 'ansChoice4') {
-    document.getElementById('ans3').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans3').append('Correct!')
-    score++
-    setInterval(() => {
-      document.getElementById('ans3').textContent = ''
-    }, 3000);
-  } else {
-    document.getElementById('ans3').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans3').append('Wrong!')
-    setInterval(() => {
-      document.getElementById('ans3').textContent = ''
-    }, 3000);
-  }
-})
-
-document.getElementById('btns4').addEventListener('click', event => {
-  if (event.target.className === 'ansChoice3') {
-    document.getElementById('ans4').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans4').append('Correct!')
-    score++
-    setInterval(() => {
-      document.getElementById('ans4').textContent = ''
-    }, 3000);
-  } else {
-    document.getElementById('ans4').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans4').append('Wrong!')
-    setInterval(() => {
-      document.getElementById('ans4').textContent = ''
-    }, 3000);
-  }
-})
-
-document.getElementById('btns5').addEventListener('click', event => {
-  if (event.target.className === 'ansChoice4') {
-    document.getElementById('ans5').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans5').append('Correct!')
-    score++
-    setInterval(() => {
-      document.getElementById('ans5').textContent = ''
-    }, 3000);
-  } else {
-    document.getElementById('ans5').innerHTML = `
-    <hr>
-    `
-    document.getElementById('ans5').append('Wrong!')
-    setInterval(() => {
-      document.getElementById('ans5').textContent = ''
-    }, 3000);
-  }
-})
-
-document.getElementById('highscores').textContent = localStorage.getItem('initials')
-
-document.getElementById('submit').addEventListener('click', event => {
-  localStorage.setItem('initial', document.getElementById('initials').value)
-  document.getElementById('highscores').textContent = localStorage.getItem('initials')
-})
+  
